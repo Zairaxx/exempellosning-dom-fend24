40b2b75ddc519c247fff3da61e3a7edddfed7344
Zairaxx <brandontsegai@gmail.com>
2024-11-14 14:20:00 +0100

    added solution

--- instruktioner.md
+++ instruktioner.md
@@ -1,6 +1,7 @@
 INSTRUKTIONER
 
 1a) Skriv ut namnen på samtliga städer som li-taggar i existerande ul-tagg (se HTML-fil) med hjälp utav Javascript.
+
 1b) Färga namnen alla franska städer blått och alla brittiska städer rött i denna lista.
 
 2a) Skriv ut samtliga städer som har en foodRanking på 8 eller högre.
--- script.js
+++ script.js
@@ -36,3 +36,62 @@ const cities = [
       foodRanking: 6
     }
   ];
+
+  //Uppgift 1
+
+  let allCitiesList = document.querySelector("#allCities");
+
+  cities.forEach(city => {
+    let li = document.createElement("li");
+
+    if(city.code === "UK"){
+        li.style.color = "red"
+    } else if (city.code === "FR") {
+        li.style.color = "blue";
+    }
+
+
+    li.textContent = city.city;
+    allCitiesList.append(li);
+  })
+
+  //Uppgift 2
+
+  let foodCitiesList = document.querySelector("#citiesForFoodies");
+
+  let rankedFoodCitiesList = document.createElement("ol");
+
+  foodCitiesList.replaceWith(rankedFoodCitiesList);
+
+  //Filtrera
+  let foodCities = cities.filter(city => city.foodRanking >= 8)
+  //Sortera 
+  foodCities.sort((a,b) => b.foodRanking-a.foodRanking);
+  //Skriv ut i DOM:en
+  foodCities.forEach( city => {
+    let li = document.createElement("li");
+    li.innerText = city.city + " - Food ranking: " + city.foodRanking + "/10"
+    rankedFoodCitiesList.append(li);
+  })
+
+  //Uppgift 3
+
+  let populationList = document.querySelector("#sortedByPopulation");
+
+  //Sortera lista
+  //Alternativ 1
+  let citiesByPopulation = [...cities];
+  citiesByPopulation.sort((a,b) => b.population-a.population)
+
+  //Alternativ 2
+  // let citiesByPopulation = cities.toSorted((a,b) => b.population-a.population)
+
+
+  //Skriva ut i DOM:en
+
+  citiesByPopulation.forEach(city => {
+    let li = document.createElement("li");
+    li.textContent = city.city + " - Population: " + city.population + " people";
+    populationList.append(li);
+  }
+  )
